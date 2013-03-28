99527dfc7225d8c8985a99c5b41d07c9cf12974a
Osipov Stanislav <stasstels@gmail.com>
2013-03-28 17:35:35 +0400

    Contributor gets lookup element corresponding to the most popular method and get it to the top of completion

--- src/ru/spbau/recommenders/plugin/MostPopularMethodContributor.java
+++ src/ru/spbau/recommenders/plugin/MostPopularMethodContributor.java
@@ -1,11 +1,15 @@
 package ru.spbau.recommenders.plugin;
 
-import com.intellij.codeInsight.completion.CompletionContributor;
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementDecorator;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
+import com.intellij.codeInsight.lookup.LookupElementRenderer;
 import com.intellij.psi.*;
+import com.intellij.util.Consumer;
+import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
 import java.util.List;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
@@ -13,6 +17,7 @@ import static ru.spbau.recommenders.plugin.PsiUtils.collectMethodCallsBeforeElem
 
 /**
  * @author Pavel Talanov
+ * @author Osipov Stanislav
  */
 public final class MostPopularMethodContributor extends CompletionContributor {
 
@@ -20,30 +25,64 @@ public final class MostPopularMethodContributor extends CompletionContributor {
     }
 
     @Override
-    public void fillCompletionVariants(CompletionParameters parameters, CompletionResultSet result) {
-        PsiElement position = parameters.getPosition();
-        //SWEET!
-        if (!psiElement(PsiIdentifier.class).withText("IntellijIdeaRulezzz")
-                .withParent(PsiReferenceExpression.class).accepts(position)) {
+    public void fillCompletionVariants(CompletionParameters parameters, final CompletionResultSet result) {
+        final String mostUsedMethodName = getMostUsedMethodName(parameters);
+        if(mostUsedMethodName == null) {
             return;
+        }
+        result.runRemainingContributors(parameters, new Consumer<CompletionResult>() {
+            @Override
+            public void consume(CompletionResult completionResult) {
+                final LookupElement lookupElement = completionResult.getLookupElement();
+                if(!lookupElement.getLookupString().equals(mostUsedMethodName)) {
+                    result.addElement(lookupElement);
+                    return;
+                }
+                addRecommendation(lookupElement);
+            }
+
+            private void addRecommendation(final LookupElement lookupElement) {
+                LookupElementDecorator<LookupElement> recommendation = LookupElementDecorator.withRenderer(lookupElement, new LookupElementRenderer<LookupElementDecorator<LookupElement>>() {
+                    @Override
+                    public void renderElement(LookupElementDecorator<LookupElement> element, LookupElementPresentation presentation) {
+                        presentation.setItemTextForeground(Color.BLUE);
+                        presentation.setItemTextBold(true);
+                        lookupElement.renderElement(presentation);
+                    }
+                });
+                result.addElement(PrioritizedLookupElement.withPriority(recommendation, 1.0));
+            }
+        });
+
+
+    }
+
+    @Nullable
+    private String getMostUsedMethodName(CompletionParameters parameters) {
+        PsiElement position = parameters.getPosition();
+        if (!psiElement(PsiIdentifier.class).withText(CompletionInitializationContext.DUMMY_IDENTIFIER_TRIMMED)
+                .withParent(PsiReferenceExpression.class).accepts(position)) {
+            return null;
         }
         PsiReferenceExpression parent = (PsiReferenceExpression) position.getParent();
         PsiExpression qualifierExpression = parent.getQualifierExpression();
         if (!(qualifierExpression instanceof PsiReferenceExpression)) {
-            return;
+            return null;
         }
         PsiType type = qualifierExpression.getType();
         if (type == null) {
-            return;
+            return null;
         }
+        return calculateMostUsedMethodName(position, (PsiReferenceExpression) qualifierExpression, type);
+    }
+
+    @Nullable
+    private String calculateMostUsedMethodName(PsiElement position, PsiReferenceExpression qualifierExpression, PsiType type) {
         String typeName = type.getCanonicalText();
         List<String> methodsCalledBeforePosition
-                = collectMethodCallsBeforeElementForQualifier(position, (PsiReferenceExpression) qualifierExpression);
-        String mostUsedMethodName =
-                MethodStatisticsProjectComponent.getInstance(position.getProject()).getMostUsedMethodName(typeName, methodsCalledBeforePosition);
-        if (mostUsedMethodName != null) {
-            result.addElement(LookupElementBuilder.create(mostUsedMethodName + "()").withTypeText("most used", true));
-        }
+                = collectMethodCallsBeforeElementForQualifier(position, qualifierExpression);
+        return MethodStatisticsProjectComponent.getInstance(position.getProject()).getMostUsedMethodName(typeName, methodsCalledBeforePosition);
+
     }
 
 }
