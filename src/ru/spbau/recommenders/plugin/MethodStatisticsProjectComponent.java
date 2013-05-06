bd2ebee1d30ee33307edc34f499f68f70ba90454
Pavel Talanov <talanov.pavel@gmail.com>
2013-05-06 12:54:11 +0400

    Draft code for maintaining old functionality

--- src/ru/spbau/recommenders/plugin/MethodStatisticsProjectComponent.java
+++ src/ru/spbau/recommenders/plugin/MethodStatisticsProjectComponent.java
@@ -1,34 +1,20 @@
 package ru.spbau.recommenders.plugin;
 
-import com.google.common.collect.Sets;
 import com.intellij.compiler.server.CustomBuilderMessageHandler;
-import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ContentIterator;
-import com.intellij.openapi.roots.ModuleFileIndex;
-import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.startup.StartupManager;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import ru.spbau.recommenders.plugin.data.Suggestions;
 import ru.spbau.jps.incremental.recommenders.RecommendersBuilder;
 import ru.spbau.jps.incremental.recommenders.StringSerializer;
+import ru.spbau.recommenders.plugin.data.Suggestions;
 import ru.spbau.recommenders.plugin.persistent.PersistentStorage;
-import ru.spbau.recommenders.plugin.psicollector.CallStatisticsCollector;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Pavel Talanov
@@ -47,7 +33,7 @@ public final class MethodStatisticsProjectComponent implements ProjectComponent 
 
     @NotNull
     private PersistentStorage storage;
-    
+
     @NotNull
     private StringSerializer<HashMap<String, Map<List<String>, Integer>>> deserializer
             = new StringSerializer<HashMap<String, Map<List<String>, Integer>>>();
@@ -72,8 +58,7 @@ public final class MethodStatisticsProjectComponent implements ProjectComponent 
                         if (builderId.equals(RecommendersBuilder.BUILDER_ID) && messageType.equals(RecommendersBuilder.MESSAGE_TYPE)) {
                             try {
                                 HashMap<String, Map<List<String>, Integer>> result = deserializer.deserialize(messageText);
-                                //TODO: process result
-                                System.out.println(result);
+                                storage.proccessDiff(result);
                             } catch (IOException e) {
                                 //TODO
                                 e.printStackTrace();
@@ -96,46 +81,11 @@ public final class MethodStatisticsProjectComponent implements ProjectComponent 
     }
 
     public void projectOpened() {
-        StartupManager.getInstance(project).registerPostStartupActivity(new Runnable() {
-            @Override
-            public void run() {
-                CallStatisticsCollector callStatisticsCollector = new CallStatisticsCollector(storage);
-                Set<PsiFile> allPsiFiles = getAllPsiFiles();
-                System.out.println(allPsiFiles);
-                for (PsiFile psiFile : allPsiFiles) {
-                    callStatisticsCollector.collectStatistics(psiFile);
-                }
-                System.out.println(storage.toString());
-            }
-        });
     }
 
     @Override
     public void projectClosed() {
         //do nothing
     }
-
-    @NotNull
-    private Set<PsiFile> getAllPsiFiles() {
-        final Set<PsiFile> files = Sets.newLinkedHashSet();
-        for (Module module : ModuleManager.getInstance(project).getModules()) {
-            final ModuleFileIndex index = ModuleRootManager.getInstance(module).getFileIndex();
-            index.iterateContent(new ContentIterator() {
-                @Override
-                public boolean processFile(VirtualFile file) {
-                    if (file.isDirectory()) return true;
-                    if (!index.isInSourceContent(file) && !index.isInTestSourceContent(file)) return true;
-
-                    final FileType fileType = FileTypeManager.getInstance().getFileTypeByFile(file);
-                    if (fileType != JavaFileType.INSTANCE) return true;
-                    PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
-                    files.add(psiFile);
-                    return true;
-                }
-            });
-        }
-        return files;
-    }
-
 
 }
