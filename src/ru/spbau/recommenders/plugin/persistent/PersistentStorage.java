accbb60187d7fcf52755466e79fa6c4935e463ec
Pavel Talanov <talanov.pavel@gmail.com>
2013-05-06 12:54:11 +0400

    Draft code for maintaining old functionality

--- src/ru/spbau/recommenders/plugin/persistent/PersistentStorage.java
+++ src/ru/spbau/recommenders/plugin/persistent/PersistentStorage.java
@@ -4,7 +4,9 @@ import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.util.Function;
 import com.intellij.util.PathUtil;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.io.PersistentHashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +16,7 @@ import ru.spbau.recommenders.plugin.storage.MethodStatisticsStorage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static ru.spbau.recommenders.plugin.persistent.Externalizers.CLASS_AND_CALL_SEQUENCE_KEY_DESCRIPTOR;
 import static ru.spbau.recommenders.plugin.persistent.Externalizers.SUGGESTIONS_EXTERNALIZER;
@@ -90,5 +93,28 @@ public final class PersistentStorage implements MethodStatisticsStorage {
         return "PersistentStorage{" +
                 "storage=" + storage +
                 '}';
+    }
+
+    //TODO: this a just a draft
+    public void proccessDiff(@NotNull Map<String, Map<List<String>, Integer>> result) {
+        for (Map.Entry<String, Map<List<String>, Integer>> stringMapEntry : result.entrySet()) {
+            String typeName = stringMapEntry.getKey();
+            String realTypeName = typeName.replace("/", ".");
+            for (Map.Entry<List<String>, Integer> sequenceAndCount : stringMapEntry.getValue().entrySet()) {
+                List<String> sequence = sequenceAndCount.getKey();
+                List<String> realSequence = ContainerUtil.map(sequence, new Function<String, String>() {
+                    @Override
+                    public String fun(String s) {
+                        return s.substring(0, s.indexOf("("));
+                    }
+                });
+                Integer count = sequenceAndCount.getValue();
+                for (int i = 0; i < sequence.size(); ++i) {
+                    for (int j = 0; j < count; ++j) {
+                        registerCallSequence(realTypeName, realSequence.subList(0, i), realSequence.get(i));
+                    }
+                }
+            }
+        }
     }
 }
